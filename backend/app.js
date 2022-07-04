52a1458148261b60beb7c3ea20ac0c43609543f3
Julia Rulova <julia.rulova0066@gmail.com>
2022-07-04 18:56:28 +0300

    fix cors 5

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,22 @@ const auth = require('./middlewares/auth');
 const errorsHandler = require('./middlewares/errorsHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'http://mesto.julia.practicum.nomoredomains.xyz',
+  'https://mesto.julia.practicum.nomoredomains.xyz',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001'
+]
+
 const NotFound = require('./errors/NotFound');
 const { linkReg } = require('./utils/constants');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.use(cors());
+app.use(cors({ origin: allowedCors }));
 
 const { login, createUser } = require('./controllers/users');
 
