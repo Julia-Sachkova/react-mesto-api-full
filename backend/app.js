3de95301a80c49b2936e74d45da94182ec2e2086
Julia Rulova <julia.rulova0066@gmail.com>
2022-07-06 15:03:18 +0300

    fix bugs

--- backend/app.js
+++ backend/app.js
@@ -34,17 +34,23 @@ const allowedCors = [
 ];
 
 app.use((req, res, next) => {
-  // const { method } = req;
+  const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Origin', origin);
+
   res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
+
   return next();
 });
 
